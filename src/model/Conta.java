a63f36108e124a6df16f0242c5d257aebcb41e6a
lucasmanhaesr <ramosmanhaes@outlook.com>
2024-12-13 13:22:25 -0300

    Aplicando polimorfismo

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -8,43 +8,9 @@ public abstract class Conta {
     private String titular;
     private float saldo;
 
-    public boolean sacar(float valor){
-        if(this.getSaldo() < valor){
-            System.out.println("Saldo insuficiente");
-            return false;
-        }
-        this.setSaldo(this.getSaldo() - valor);
-        return true;
-    }
-
-    public void depositar(float valor){
-        this.setSaldo(this.getSaldo() + valor);
-    }
-
-    public void visualizar(){
-        String tipo = "";
-
-        switch(this.tipo){
-            case 1:
-                tipo = "Conta Corrente";
-                break;
-            case 2:
-                tipo = "Conta Poupanca";
-                break;
-        }
-        System.out.println("\n\n***************************************************");
-        System.out.println("                  EXTRATO BANCÁRIO");
-        System.out.println("***************************************************");
-        System.out.println("Numero da Conta: " + this.numero);
-        System.out.println("Agência: " + this.agencia);
-        System.out.println("Tipo da Conta: " + tipo);
-        System.out.println("Titular: " + this.titular);
-        System.out.println("Saldo: " + this.saldo);
-        System.out.println("***************************************************\n\n");
-    }
-
     //############ BOILERPLATE #####################
     //Constructor
+    public Conta(){}
     public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
         this.numero = numero;
         this.agencia = agencia;
@@ -89,4 +55,39 @@ public abstract class Conta {
         this.agencia = agencia;
     }
     //###############################################
+
+    public boolean sacar(float valor){
+        if(this.getSaldo() < valor){
+            System.out.println("Saldo insuficiente");
+            return false;
+        }
+        this.setSaldo(this.getSaldo() - valor);
+        return true;
+    }
+
+    public void depositar(float valor){
+        this.setSaldo(this.getSaldo() + valor);
+    }
+
+    public void visualizar(){
+        String tipo = "";
+
+        switch(this.tipo){
+            case 1:
+                tipo = "Conta Corrente";
+                break;
+            case 2:
+                tipo = "Conta Poupanca";
+                break;
+        }
+        System.out.println("\n\n***************************************************");
+        System.out.println("                  EXTRATO BANCÁRIO");
+        System.out.println("***************************************************");
+        System.out.println("Numero da Conta: " + this.numero);
+        System.out.println("Agência: " + this.agencia);
+        System.out.println("Tipo da Conta: " + tipo);
+        System.out.println("Titular: " + this.titular);
+        System.out.println("Saldo: " + this.saldo);
+        System.out.println("***************************************************\n\n");
+    }
 }
--- src/model/ContaCorrente.java
+++ src/model/ContaCorrente.java
@@ -9,10 +9,41 @@ public class ContaCorrente extends Conta{
         this.limite = limite;
     }
 
+    //Sobrecarga
+    public ContaCorrente(int numero, int agencia, int tipo, String titular, float saldo) {
+        super(numero, agencia, tipo, titular, saldo);
+        this.limite = 500;
+    }
+
+
     public float getLimite() {
         return limite;
     }
     public void setLimite(float limite) {
         this.limite = limite;
+    }
+
+    //Sobreescrita
+    public void visualizar(){
+        String tipo = "";
+
+        switch(super.getTipo()){
+            case 1:
+                tipo = "Conta Corrente";
+                break;
+            case 2:
+                tipo = "Conta Poupanca";
+                break;
+        }
+        System.out.println("\n\n***************************************************");
+        System.out.println("                  EXTRATO BANCÁRIO");
+        System.out.println("***************************************************");
+        System.out.println("Numero da Conta: " + super.getNumero());
+        System.out.println("Agência: " + super.getAgencia());
+        System.out.println("Tipo da Conta: " + tipo);
+        System.out.println("Titular: " + super.getTitular());
+        System.out.println("Saldo: " + super.getSaldo());
+        System.out.println("Limite: " + getLimite());
+        System.out.println("***************************************************\n\n");
     }
 }
